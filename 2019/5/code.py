e29864a7d2c6c9ee1466fc902932b2a8c9bdf74d
Bryan Klimt <bklimt@gmail.com>
2019-12-07 15:30:22 -0800

    5.1

--- 2019/5/code.py
+++ 2019/5/code.py
@@ -1,12 +1,11 @@
 import sys
 
-program = [1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,6,1,19,2,19,13,23,1,23,10,27,1,13,27,31,2,31,10,35,1,35,9,39,1,39,13,43,1,13,43,47,1,47,13,51,1,13,51,55,1,5,55,59,2,10,59,63,1,9,63,67,1,6,67,71,2,71,13,75,2,75,13,79,1,79,9,83,2,83,10,87,1,9,87,91,1,6,91,95,1,95,10,99,1,99,13,103,1,13,103,107,2,13,107,111,1,111,9,115,2,115,10,119,1,119,5,123,1,123,2,127,1,127,5,0,99,2,14,0,0]
-
 class Computer:
   def __init__(self, memory):
     self.running = True
     self.memory = memory
     self.ip = 0
+    self.modes = 0
 
   def log(self, s):
     print(s)
@@ -15,47 +14,71 @@ class Computer:
     self.log("exiting")
     self.running = False
 
-  def add(self, a1, a2, a3):
-    d1 = self.memory[a1]
-    d2 = self.memory[a2]
+  def add(self, d1, d2, a3):
     d3 = d1 + d2
     self.memory[a3] = d3
-    self.log("[%d] %d + [%d] %d = [%d] %d" % (a1, d1, a2, d2, a3, d3))
+    self.log("[%d] = %d + %d = %d" % (a3, d1, d2, d3))
 
-  def multiply(self, a1, a2, a3):
-    d1 = self.memory[a1]
-    d2 = self.memory[a2]
+  def multiply(self, d1, d2, a3):
     d3 = d1 * d2
     self.memory[a3] = d3
-    self.log("[%d] %d * [%d] %d = [%d] %d" % (a1, d1, a2, d2, a3, d3))
+    self.log("[%d] = %d * %d = %d" % (a3, d1, d2, d3))
 
   def input(self, a1):
-    d1 = 0
+    self.log('input: ')
+    d1 = input()
     self.memory[a1] = d1
-    self.log("[%a] = %d", (a1, d1))
+    self.log("[%d] = %d" % (a1, d1))
+
+  def output(self, d1):
+    self.log('output: %d' % d1)
 
   def fetch(self):
     d1 = self.memory[self.ip]
     self.ip = self.ip + 1
     return d1
 
+  def fetchop(self):
+    op = self.fetch()
+    opcode = op % 100
+    op = op / 100
+    self.modes = op
+    return opcode
+
+  def fetchdata(self):
+    mode = self.modes % 10
+    self.modes = self.modes / 10
+    d1 = self.fetch()
+    if mode == 0:
+      return self.memory[d1]
+    return d1
+
+  def fetchaddr(self):
+    mode = self.modes % 10
+    self.modes = self.modes / 10
+    d1 = self.fetch()
+    return d1
+
   def process(self):
-    opcode = self.fetch()
+    opcode = self.fetchop()
     if opcode == 99:
       self.quit()
     elif opcode == 1:
-      a = self.fetch()
-      b = self.fetch()
-      c = self.fetch()
+      a = self.fetchdata()
+      b = self.fetchdata()
+      c = self.fetchaddr()
       self.add(a, b, c)
     elif opcode == 2:
-      a = self.fetch()
-      b = self.fetch()
-      c = self.fetch()
+      a = self.fetchdata()
+      b = self.fetchdata()
+      c = self.fetchaddr()
       self.multiply(a, b, c)
     elif opcode == 3:
-      a = self.fetch()
+      a = self.fetchaddr()
       self.input(a)
+    elif opcode == 4:
+      a = self.fetchdata()
+      self.output(a)
     else:
       self.log("unknown opcode: %d" % opcode)
       self.quit()
@@ -66,7 +89,7 @@ class Computer:
     self.ip = 0
     while self.running:
       self.process()
-      self.log('state: %s' % self.memory)
+      # self.log('state: %s' % self.memory)
     self.log("halted.")
     return self.memory[0]
 
@@ -83,18 +106,22 @@ def patch(m, n, v):
   m[2] = v
   return m
 
-def main():
+def test():
   print(run([1,9,10,3,2,3,11,0,99,30,40,50]))
   print(run([1,0,0,0,99]))
   print(run([2,3,0,3,99]))
   print(run([2,4,4,5,99,0]))
   print(run([1,1,1,4,99,5,6,0,99]))
-  for n in range(100):
-    for v in range(100):
-      result = run(patch(program, n, v))
-      print("result: %d, noun: %d, verb: %d" % (result, n, v))
-      if result == 19690720:
-        sys.exit(0)
+  print(run([3,0,4,0,99]))
+  print(run([1002,4,3,4,33]))
+  print(run([1101,100,-1,4,0]))
+
+def main():
+  f = file('program.txt')
+  s = f.read().strip()
+  ss = s.split(',')
+  xs = [int(x) for x in ss]
+  print(run(xs))
 
 main()
 
