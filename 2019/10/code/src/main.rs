4c8f4fed0d1ae583299952d664c83c3c2213a083
Bryan Klimt <bklimt@gmail.com>
2019-12-10 16:31:47 -0800

    10.1

--- 2019/10/code/src/main.rs
+++ 2019/10/code/src/main.rs
@@ -1,9 +1,9 @@
 
-use std::collections::HashSet;
-use std::fs;
-use vector2d::Vector2D;
-
 extern crate num_rational;
+
+use std::collections::HashSet;
+use std::env;
+use std::fs;
 
 fn print_map(map: &Vec<Vec<i32>>) {
     for row in 0..map.len() {
@@ -19,7 +19,10 @@ fn print_map(map: &Vec<Vec<i32>>) {
 }
 
 fn main() {
-    let text = fs::read_to_string("input1.txt")
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+
+    let text = fs::read_to_string(filename)
         .expect("unable to read file");
 
     // Determine the dimensions.
@@ -54,21 +57,49 @@ fn main() {
     println!("");
 
     // Build up the result.
+    let mut max = 0;
+    let mut max_row = 0;
+    let mut max_col = 0;
     let mut result = vec![vec![0; cols]; rows];
     for row in 0..rows {
         for col in 0..cols {
-            println!("{},{}:", row, col);            
+            // println!("{},{}:", row, col);            
             if map[row][col] == 1 {
                 let mut seen = HashSet::new();
                 // Iterate over all the other asteroids.
                 for r in 0..rows {
                     for c in 0..cols {
+                        if r == row && c == col {
+                            continue;
+                        }
                         if map[r][c] == 1 {
                             // Check if it's visible.
-                            let dy = (r as i32) - (row as i32);
-                            let dx = (c as i32) - (col as i32);
-                            // Normalize the vector.
-                            println!("  {},{}: vec: {:?}", r, c, t);
+
+                            // Create a normalized vector pointing at the target.
+                            let mut dy = (r as i32) - (row as i32);
+                            let mut dx = (c as i32) - (col as i32);
+                            // println!("       dx: {}, dy: {}", dx, dy);
+                            let signx = dx.signum();
+                            let signy = dy.signum();
+                            // println!("       sx: {}, sy: {}", signx, signy);
+                            dx = signx * dx;
+                            dy = signy * dy;
+                            if dx == 0 {
+                                dy = 1;
+                            } else if dy == 0 {
+                                dx = 1;
+                            } else {
+                                let r = num_rational::Ratio::new(dx, dy);
+                                dx = *r.numer();
+                                dy = *r.denom();
+                                // println!("      *dx: {},*dy: {}", dx, dy);
+                            }
+                            dx = signx * dx;
+                            dy = signy * dy;
+                            // println!("      +dx: {},+dy: {}", dx, dy);
+                            let t = (dx, dy);
+
+                            // println!("  {},{}: vec: {:?}", r, c, t);
                             if !seen.contains(&t) {
                                 seen.insert(t);
                                 result[row][col] = result[row][col] + 1;
@@ -76,10 +107,18 @@ fn main() {
                         }
                     }
                 }
+                let visible = result[row][col];
+                if visible > max {
+                    max = visible;
+                    max_row = row;
+                    max_col = col;
+                }
             }
         }
     }
     print_map(&result);
 
-    // println!("{:?}", map);
+    println!("max: {} at [{}, {}]", max, max_row, max_col);
 }
+
+// 230
