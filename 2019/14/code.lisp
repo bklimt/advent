300476cafbea167496976a4544caedba373a01bd
Bryan Klimt <bklimt@gmail.com>
2019-12-13 23:52:23 -0800

    14.1 - check progress

--- 2019/14/code.lisp
+++ 2019/14/code.lisp
@@ -13,8 +13,8 @@
 
 (defun parse-equation (s)
   (let ((i (search " => " s)))
-    (list (parse-list (subseq s 0 i))
-          (parse-item (subseq s (+ i 4))))))
+    (list (parse-item (subseq s (+ i 4)))
+          (parse-list (subseq s 0 i)))))
 
 (defun read-lines (f)
   (let ((line (read-line f nil)))
@@ -27,4 +27,27 @@
     (close f)
     lines))
 
+(defun count-ore-for-element (element rules)
+  ; (format t "searching for: ~A in rules: ~A~%" element rules)
+  (if rules
+      ; Check the first rule.
+      (let* ((name (cadr element))
+             (amount (car element))
+             (rule (car rules))
+             (lhs (car rule))
+             (rule-name (cadr lhs))
+             (rule-amount (car lhs)))
+            (format t "checking for: ~A in rule: ~A~%" element rule)
+            (if (eq rule-name name)
+              (format t "   YES!~%"))
+            ; Check the rest of the rules.
+            (count-ore-for-element element (cdr rules)))))
+
+(defun count-ore-for-elements (elements rules)
+  (if elements
+      (cons (count-ore-for-element (car elements) rules)
+            (count-ore-for-elements (cdr elements) rules))))
+
 (setq rules (read-file "input1.txt"))
+
+(setq ore (count-ore-for-elements '((1 FUEL)) rules))
