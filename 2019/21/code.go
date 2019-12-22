2c54e220b2915e96dce8cd02a0332bd1658ab0e7
Bryan Klimt <bklimt@gmail.com>
2019-12-21 19:47:34 -0800

    21.1

---
+++ 2019/21/code.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+)
+
+type position struct {
+	X int
+	Y int
+}
+
+func main() {
+	p := ReadProgram()
+	in := make(chan int)
+	out := make(chan int)
+	wantInput := make(chan bool)
+	c := NewComputer(p, in, out, wantInput)
+	go func() {
+		c.Run()
+	}()
+
+	// Supply the input programming.
+	go func() {
+		// NOT C J
+		// AND D J
+		// NOT A T
+		// OR T J
+		s := "NOT C J\nAND D J\nNOT A T\nOR T J\nRUN\n"
+		for _, c := range s {
+			<-wantInput
+			in <- int(c)
+		}
+		close(in)
+	}()
+
+	screen := make(map[position]int)
+	cursor := position{}
+	size := position{}
+
+	for {
+		c, ok := <-out
+		if !ok {
+			break
+		}
+
+		if c > 127 {
+			fmt.Printf("\nRESULT: %d\n", c)
+			break
+		}
+
+		fmt.Printf("%c", c)
+		screen[cursor] = c
+		if c == 10 {
+			cursor.X = 0
+			cursor.Y++
+		} else {
+			cursor.X++
+		}
+		if cursor.X > size.X {
+			size.X = cursor.X
+		}
+		if cursor.Y > size.Y {
+			size.Y = cursor.Y
+		}
+	}
+	close(in)
+
+	fmt.Println()
+	fmt.Println()
+}
+
+// 1:
+// NOT C J
+// AND D J
+// NOT A T
+// OR T J
+// WALK
+// 19357180
+
+// 2:
