62a597a0b3e145644cca6d367ee748e99da3fe25
Bryan Klimt <bklimt@gmail.com>
2020-12-13 23:36:36 -0800

    day 13

--- 2020/13/problem2.py
+++ 2020/13/problem2.py
@@ -1,65 +1,19 @@
 
-input = file('sample.txt').read().strip().split()
+input = file('input.txt').read().strip().split()
 
 busses = input[1].split(',')
 busses = [[i, int(busses[i])] for i in range(len(busses)) if busses[i] != 'x']
+busses = [[bus[0] % bus[1], bus[1]] for bus in busses]
 print 'busses = ' + repr(busses)
-
-#busses[0][0] = busses[0][1]
-busses = busses[0:2]
-print 'busses = ' + repr(busses)
-
-def is_solution(target):
-  # x % -b == -i
-  # return all([-b[0] == target % -b[1] for b in busses])
-  # x % b == b - i
-  return all([b[1] - b[0] == target % b[1] for b in busses])
-
-def find_pairwise(p1, p2):
-  i = 1
-  while True:
-    for j in range(i+1):
-      print '%s -> %s' % ([j, i], [p1[1]*i+p1[0], p2[1]*j+p2[0]])
-      if p1[1]*i+p1[0] == p2[1]*j+p2[0]:
-        return [j, i]
-    i = i + 1
-    if i > 25:
-      return False
-
-def find_t(factor1, offset1, factor2):
-  y1 = factor1 - offset1
-  y2 = factor1 * 2 - offset1
-  mod1 = y1 % factor2
-  mod2 = y2 % factor2
-  diff = mod2 - mod1
-  print y1, y2, mod1, mod2, diff
 
 def main():
-  largest = max(busses, key = lambda b: b[1])
-  print 'largest = ' + repr(largest)
-  i = largest[1] - largest[0]
-  while True:
-    if is_solution(i):
-      print 'ans = ' + repr(i)
-      return
-    i = i + largest[1]
+  t = busses[0][0]
+  dt = busses[0][1]
+  for bus in busses[1:]:
+    while -bus[0] != t % -bus[1]:
+      t += dt
+    dt = dt * bus[1]
+    print 'caught bus at t=' + repr(t) + ', dt=' + repr(dt)
+  print t
 
-# print is_solution(1068780)
-print is_solution(1068781)
-# print is_solution(1068782)
-
-# main()
-
-# [[7, 7], [1, 13], [4, 59], [6, 31], [7, 19]]
-
-# 7 * x -t = 7
-# 13 * y - t = 1
-# 59 * z - t = 5
-# 31 * v - t = 6
-# 19 * w - t = 7
-
-# print find_pairwise([0, 7], [1, 13])
-
-# main()
-
-find_t(13, 1, 7)
+main()
