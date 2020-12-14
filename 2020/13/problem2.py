0b3c7c4df9222337b6b4dcbca2e5acf486b7946d
Bryan Klimt <bklimt@gmail.com>
2020-12-13 21:19:42 -0800

    day 13 part 2 take 2

--- 2020/13/problem2.py
+++ 2020/13/problem2.py
@@ -1,13 +1,12 @@
 
-import numpy
-
-input = file('input.txt').read().strip().split()
+input = file('sample.txt').read().strip().split()
 
 busses = input[1].split(',')
 busses = [[i, int(busses[i])] for i in range(len(busses)) if busses[i] != 'x']
 print 'busses = ' + repr(busses)
 
-busses[0][0] = busses[0][1]
+#busses[0][0] = busses[0][1]
+busses = busses[0:2]
 print 'busses = ' + repr(busses)
 
 def is_solution(target):
@@ -15,6 +14,25 @@ def is_solution(target):
   # return all([-b[0] == target % -b[1] for b in busses])
   # x % b == b - i
   return all([b[1] - b[0] == target % b[1] for b in busses])
+
+def find_pairwise(p1, p2):
+  i = 1
+  while True:
+    for j in range(i+1):
+      print '%s -> %s' % ([j, i], [p1[1]*i+p1[0], p2[1]*j+p2[0]])
+      if p1[1]*i+p1[0] == p2[1]*j+p2[0]:
+        return [j, i]
+    i = i + 1
+    if i > 25:
+      return False
+
+def find_t(factor1, offset1, factor2):
+  y1 = factor1 - offset1
+  y2 = factor1 * 2 - offset1
+  mod1 = y1 % factor2
+  mod2 = y2 % factor2
+  diff = mod2 - mod1
+  print y1, y2, mod1, mod2, diff
 
 def main():
   largest = max(busses, key = lambda b: b[1])
@@ -40,5 +58,8 @@ print is_solution(1068781)
 # 31 * v - t = 6
 # 19 * w - t = 7
 
+# print find_pairwise([0, 7], [1, 13])
 
-print ans
+# main()
+
+find_t(13, 1, 7)
