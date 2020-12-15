6b07cb7c6eddffe4ba27c358563bff9655f8d7b4
Bryan Klimt <bklimt@gmail.com>
2020-12-14 21:35:52 -0800

    day 15

---
+++ 2020/15/problem.py
@@ -0,0 +1,32 @@
+
+input = [0,12,6,13,20,1,17]
+
+def game(n):
+  seen = {
+  }
+
+  i = 0
+
+  curr = 0
+  for x in input:
+    prev = curr
+    curr = x
+    if i > 0:
+      seen[prev] = i - 1
+    i = i + 1
+    # print `i` + ' -> ' + `curr` + ': ' + `seen`
+
+  while i < n:
+    prev = curr
+    if curr not in seen:
+      curr = 0
+    else:
+      curr = (i - seen[curr]) - 1
+    seen[prev] = i - 1
+    i = i + 1
+    # print `i` + ' -> ' + `curr` + ': ' + `seen`
+
+  print curr
+
+game(2020)
+game(30000000)
