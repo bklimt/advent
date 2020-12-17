02b8a8dd560c927098a1c7340c903e33bd6e5d3f
Bryan Klimt <bklimt@gmail.com>
2020-12-16 22:41:48 -0800

    day 17 part 1

---
+++ 2020/17/src/problem1.cc
@@ -0,0 +1,141 @@
+
+#include <cstdio>
+#include <cstdlib>
+#include <map>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+using std::string;
+using std::vector;
+
+class Space {
+ public:
+  Space(): minx_(0), maxx_(0), miny_(0), maxy_(0), minz_(0), maxz_(0), count_(0) {}
+
+  void Set(int x, int y, int z) {
+    if (x < minx_) minx_ = x;
+    if (y < miny_) miny_ = y;
+    if (z < minz_) minz_ = z;
+    if (x > maxx_) maxx_ = x;
+    if (y > maxy_) maxy_ = y;
+    if (z > maxz_) maxz_ = z;
+    data_[std::make_tuple(x, y, z)] = true;
+    count_++;
+  }
+
+  bool Get(int x, int y, int z) const {
+    auto it = data_.find(std::make_tuple(x, y, z));
+    if (it == data_.end()) {
+      return false;
+    }
+    return true;
+  }
+
+  int minx() const { return minx_; }
+  int miny() const { return miny_; }
+  int minz() const { return minz_; }
+  int maxx() const { return maxx_; }
+  int maxy() const { return maxy_; }
+  int maxz() const { return maxz_; }
+
+  int count() const { return count_; }
+
+ private:
+  int count_;
+  int minx_, miny_, minz_, maxx_, maxy_, maxz_;
+  std::map<std::tuple<int, int, int>, bool> data_;
+};
+
+void ReadLayer(const char *path, Space *space) {
+  FILE *f = fopen(path, "r");
+  if (f == nullptr) {
+    printf("unable to open file");
+    exit(-1);
+  }
+  int x = 0;
+  int y = 0;
+  int c;
+  while ((c = fgetc(f)) != EOF) {
+    if (c == '\n') {
+      x = 0;
+      y++;
+    } else {
+      if (c == '#') {
+        space->Set(x, y, 0);
+      }
+      x++;
+    }
+  }
+}
+
+void PrintSpace(const Space &space) {
+  for (int z = space.minz(); z <= space.maxz(); z++) {
+    printf("z = %d\n", z);
+    for (int y = space.miny(); y <= space.maxy(); y++) {
+      for (int x = space.minx(); x <= space.maxx(); x++) {
+        if (space.Get(x, y, z)) {
+          printf("#");
+        } else {
+          printf(".");
+        }
+      }
+      printf("\n");
+    }
+    printf("\n");
+  }
+}
+
+int CountNeighbors(const Space& input, int x, int y, int z) {
+  int count = 0;
+  for (int dx = -1; dx <= 1; dx++) {
+    for (int dy = -1; dy <= 1; dy++) {
+      for (int dz = -1; dz <= 1; dz++) {
+        if (dx != 0 || dy != 0 || dz != 0) {
+          if (input.Get(x + dx, y + dy, z + dz)) {
+            count++;
+          }          
+        }
+      }
+    }
+  }
+  return count;
+}
+
+std::unique_ptr<Space> Process(const Space& input) {
+  auto output = std::make_unique<Space>();
+  for (int z = input.minz() - 1; z <= input.maxz() + 1; z++) {
+    for (int y = input.miny() - 1; y <= input.maxy() + 1; y++) {
+      for (int x = input.minx() - 1; x <= input.maxx() + 1; x++) {
+        bool old = input.Get(x, y, z);
+        int n = CountNeighbors(input, x, y, z);
+        if (old) {
+          if (n == 2 || n == 3) {
+            output->Set(x, y, z);
+          }
+        } else {
+          if (n == 3) {
+            output->Set(x, y, z);
+          }
+        }
+      }
+    }
+  }
+  return output;
+}
+
+int main(int argc, char **argv) {
+  std::unique_ptr<Space> space = std::make_unique<Space>();
+  ReadLayer("input.txt", space.get());
+  // PrintSpace(*space);
+
+  for (int i = 0; i < 6; i++) {
+    space = std::move(Process(*space));
+    printf("After %d steps...\n", i + 1);
+    // PrintSpace(*space);
+    printf("Count: %d\n\n", space->count());
+  }
+
+  return 0;
+}
