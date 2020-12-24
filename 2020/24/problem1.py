1ab602e0d73ed644623ba2bc7aaf3891ba28b849
Bryan Klimt <bklimt@gmail.com>
2020-12-23 23:15:39 -0800

    day 24

--- 2020/24/problem1.py
+++ 2020/24/problem1.py
@@ -1,6 +1,5 @@
 
 lines = file('input.txt').read().strip().split()
-tiles = {}
 
 def follow(line):
   x = 0
@@ -29,20 +28,74 @@ def follow(line):
       x = x - 2
   return x, y
 
+class Board:
+  def __init__(self):
+    self.tiles = {}
+    self.minX = 0
+    self.minY = 0
+    self.maxX = 0
+    self.maxY = 0
+
+  def set(self, x, y, v):
+    if x < self.minX:
+      self.minX = x
+    if y < self.minY:
+      self.minY = y
+    if x >= self.maxX:
+      self.maxX = x + 1
+    if y >= self.maxY:
+      self.maxY = y + 1
+    if v:
+      self.tiles[(x, y)] = True
+    elif (x, y) in self.tiles:
+      del self.tiles[(x, y)]
+
+  def toggle(self, x, y):
+    self.set(x, y, not self.check(x, y))
+
+  def count(self):
+    count = 0
+    for k in self.tiles:
+      if self.tiles[k]:
+        count = count + 1
+    return count
+
+  def check(self, x, y):
+    return ((x, y) in self.tiles) and self.tiles[(x, y)]
+
+  def cycle(self):
+    b = Board()
+    for x in range(self.minX - 2, self.maxX + 2):
+      for y in range(self.minY - 1, self.maxY + 1):
+        neighbor_xy = [
+          (x-2, y),
+          (x+2, y),
+          (x-1, y-1),
+          (x-1, y+1),
+          (x+1, y-1),
+          (x+1, y+1),
+        ]
+        neighbors = [self.check(c[0], c[1]) for c in neighbor_xy]
+        on_neighbors = len([n for n in neighbors if n])
+
+        if self.check(x, y):
+          if on_neighbors == 1 or on_neighbors == 2:
+            b.set(x, y, True)
+        else:
+          if on_neighbors == 2:
+            b.set(x, y, True)
+    return b
+
 def main():
+  board = Board()
   for line in lines:
     x, y = follow(line)
-    if (x, y) not in tiles:
-      tiles[(x, y)] = True
-    else:
-      tiles[(x, y)] = not tiles[(x, y)]
+    board.toggle(x, y)
+  print board.count()
 
-  count = 0
-  for k in tiles:
-    if tiles[k]:
-      count = count + 1
-
-  print count
+  for i in range(100):
+    board = board.cycle()
+    print (i+1, board.count())
 
 # print follow('nwwswee')
 
