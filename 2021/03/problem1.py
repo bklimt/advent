a46bc2de428ad0165ebc7cc644cb5049dd3a0ecd
Bryan Klimt <bklimt@gmail.com>
2021-12-03 21:36:22 -0800

    2021 day 3 part 2

--- 2021/03/problem1.py
+++ 2021/03/problem1.py
@@ -1,27 +1,55 @@
 
-counts = []
-lines = 0
+import math
 
-f = open('input.txt')
-for line in f:
-  line = line.strip()
-  lines = lines + 1
-  for i, bit in enumerate(line):
-    while i >= len(counts):
-      counts.append(0)
-    if bit == '1':
-      counts[i] = counts[i] + 1
-  print(line)
-  print(counts)
+def mcb(lines):
+  #print('lines =', lines)
+  counts = []
+  for line in lines:
+    for i, bit in enumerate(line):
+      while i >= len(counts):
+        counts.append(0)
+      if bit == '1':
+        counts[i] = counts[i] + 1
+  target = math.ceil(len(lines)/2)
+  #print('counts =', counts)
+  #print('target =', target)
+  return ''.join(['1' if x >= target else '0' for x in counts])
 
-  target = round(lines/2)
+def inverse(n):
+  return ''.join(['0' if x == '1' else '1' for x in n])
 
-  gamma = ''.join(['1' if x >= target else '0' for x in counts])
-  epsilon = ''.join(['1' if x < target else '0' for x in counts])
+def part2(lines, inv, i=0):
+  #print('lines =', lines)
+  if len(lines) == 1:
+    return lines[0]
+  if len(lines) == 0:
+    return None
+  pattern = mcb(lines)
+  #print('pattern =', pattern)
+  if inv:
+    pattern = inverse(pattern)
+  #print('pattern =', pattern, 'inv =', inv, 'i =', i)
+  matching = [line for line in lines if line[i] == pattern[i]]
+  return part2(matching, inv, i+1)
 
+def main():
+  f = open('input.txt')
+  lines = [line.strip() for line in f if line.strip() != '']
+  gamma = mcb(lines)
+  epsilon = inverse(gamma)
   gamma = int(gamma, 2)
   epsilon = int(epsilon, 2)
+  print('gamma =', gamma)
+  print('epsilon =', epsilon)
+  print('ans1 =', gamma * epsilon)
+  oxygen = part2(lines, False)
+  co2 = part2(lines, True)
+  oxygen = int(oxygen, 2)
+  co2 = int(co2, 2)
+  print('oxygen =', oxygen)
+  print('co2 =', co2)
+  print('ans2 =', oxygen * co2)
 
-  print(gamma)
-  print(epsilon)
-  print(gamma * epsilon)
+main()
+
+#print(mcb(['00100', '01111', '00111', '00010', '01010']))
