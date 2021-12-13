07f9d0fd4ae8fd607e4ee6107615f930bdc94f33
Bryan Klimt <bklimt@gmail.com>
2021-12-12 16:01:33 -0800

    2021 day 10 part 2

--- 2021/10/part1.cc
+++ 2021/10/part1.cc
@@ -8,7 +8,7 @@
 #include "2021/base/util.h"
 #include "absl/status/status.h"
 
-absl::StatusOr<int> ScoreLine(absl::string_view s) {
+absl::StatusOr<std::pair<int, int64_t>> ScoreLine(absl::string_view s) {
   std::vector<char> stack;
 
 #define SWITCH_CASE(open, close, score)                                  \
@@ -19,7 +19,7 @@ absl::StatusOr<int> ScoreLine(absl::string_view s) {
     if (stack.back() != open) {                                          \
       std::cout << s << " - Expected " << stack.back() << ", but found " \
                 << close << " instead." << std::endl;                    \
-      return score;                                                      \
+      return std::make_pair(score, 0);                                   \
     }                                                                    \
     stack.pop_back();                                                    \
     break
@@ -35,26 +35,59 @@ absl::StatusOr<int> ScoreLine(absl::string_view s) {
             absl::StrCat("invalid character: ", static_cast<int>(c)));
     }
   }
-
 #undef SWITCH_CASE
-  return 0;
+
+  int64_t part2 = 0;
+  while (!stack.empty()) {
+    part2 *= 5;
+    char c = stack.back();
+    switch (c) {
+      case '(':
+        part2 += 1;
+        break;
+      case '[':
+        part2 += 2;
+        break;
+      case '{':
+        part2 += 3;
+        break;
+      case '<':
+        part2 += 4;
+        break;
+    }
+    stack.pop_back();
+  }
+
+  std::cout << s << " - " << part2 << " total points" << std::endl;
+
+  return std::make_pair(0, part2);
 }
 
 absl::Status Main() {
   ASSIGN_OR_RETURN(auto in, OpenFile("2021/10/input.txt"));
 
   int part1 = 0;
+  std::vector<int64_t> part2s;
 
   auto line = ReadLine(in);
   while (line) {
     if (*line != "") {
-      ASSIGN_OR_RETURN(int score, ScoreLine(*line));
-      part1 += score;
+      ASSIGN_OR_RETURN(auto scores, ScoreLine(*line));
+      part1 += scores.first;
+      if (scores.second != 0) {
+        part2s.push_back(scores.second);
+      }
     }
     line = ReadLine(in);
   }
 
+  std::sort(part2s.begin(), part2s.end());
+  int64_t part2 = part2s[part2s.size() / 2];
+
   std::cout << "Part 1: " << part1 << std::endl;
+  std::cout << "Part 2: " << part2 << std::endl;
+
+  // 70880594 is too low.
 
   return absl::OkStatus();
 }
