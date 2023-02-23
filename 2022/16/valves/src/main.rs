0ab3fda4c8cb3a5fdd184f11ac2cb2da6563d9bb
Bryan Klimt <bklimt@gmail.com>
2023-02-22 17:48:21 -0800

    day 16 part 2

--- 2022/16/valves/src/main.rs
+++ 2022/16/valves/src/main.rs
@@ -1,6 +1,5 @@
 use anyhow::{anyhow, Context, Result};
 use clap::Parser;
-use itertools::Itertools;
 use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
@@ -141,90 +140,77 @@ fn build_adj(valves: &HashMap<String, Valve>, debug: bool) -> HashMap<(String, S
     adj
 }
 
-fn dfs_search(
-    valves: &HashMap<String, Valve>,
-    adj: &HashMap<(String, String), i32>,
-    current: &String,
+struct Path {
+    path: Vec<String>,
+    time: i32,
     flow: i32,
-    time_remaining: i32,
-    open: &mut HashSet<String>,
-    debug: bool,
-) -> i32 {
-    if time_remaining < 0 {
-        panic!("invalid time_remaining: {}", 0);
-    }
-    if time_remaining == 0 {
-        if debug {
-            println!("skipping {} with time = 0", current);
-        }
-        return 0;
-    }
+    total: i32,
+}
 
-    if debug {
-        println!("visiting {} with time = {}", current, time_remaining);
+impl Path {
+    fn score(&self, time: i32) -> i32 {
+        self.total + (self.flow * (time - self.time))
     }
+}
 
-    // The default is to do nothing.
-    let mut best = flow * time_remaining;
-
-    for (_, next) in valves.iter() {
-        if next.rate == 0 {
-            continue;
-        }
-        if open.contains(&next.name) {
-            continue;
-        }
+fn extend(
+    path: &Path,
+    next: &Valve,
+    max_time: i32,
+    seen: &HashSet<String>,
+    adj: &HashMap<(String, String), i32>,
+) -> Option<Path> {
+    return if next.rate == 0 {
+        None
+    } else if seen.contains(&next.name) {
+        None
+    } else {
+        let current = path.path.last().unwrap();
         let edge = (current.clone(), next.name.clone());
         if let Some(dist) = adj.get(&edge) {
             let cost = dist + 1;
-            let new_time_remaining = time_remaining - cost;
-            if new_time_remaining < 0 {
-                continue;
+            let new_time = path.time + cost;
+            if new_time >= max_time {
+                None
+            } else {
+                // It passed all the tests. Try it.
+                let new_total = path.total + (cost * path.flow);
+                let new_flow = path.flow + next.rate;
+
+                let mut new_path = path.path.clone();
+                new_path.push(next.name.clone());
+
+                Some(Path {
+                    path: new_path,
+                    time: new_time,
+                    flow: new_flow,
+                    total: new_total,
+                })
             }
-
-            // It passed all the tests. Try it.
-            let addition = cost * flow;
-            let new_flow = flow + next.rate;
-
-            open.insert(next.name.clone());
-            best = best.max(
-                addition
-                    + dfs_search(
-                        valves,
-                        adj,
-                        &next.name,
-                        new_flow,
-                        new_time_remaining,
-                        open,
-                        debug,
-                    ),
-            );
-            open.remove(&next.name);
+        } else {
+            None
         }
-    }
-
-    if debug {
-        println!("returning best flow = {}", best);
-    }
-    best
+    };
 }
 
-fn bfs_search(
+fn bfs_search1(
     valves: &HashMap<String, Valve>,
     adj: &HashMap<(String, String), i32>,
+    max_time: i32,
     debug: bool,
 ) -> i32 {
     struct Candidate {
-        path: Vec<String>,
-        time: i32,
-        flow: i32,
-        total: i32,
+        path: Path,
+        seen: HashSet<String>,
     }
     let empty = Candidate {
-        path: vec!["AA".to_string()],
-        time: 0,
-        flow: 0,
-        total: 0,
+        path: Path {
+            path: vec!["AA".to_string()],
+            time: 0,
+            flow: 0,
+            total: 0,
+        },
+        seen: HashSet::new(),
     };
     let mut candidates = Vec::new();
     candidates.push(empty);
@@ -241,49 +227,136 @@ fn bfs_search(
             if debug {
                 println!(
                     "Considering {} [time={}, flow={}, total={}]",
-                    candidate.path.join(" -> "),
-                    candidate.time,
-                    candidate.flow,
-                    candidate.total,
+                    candidate.path.path.join(" -> "),
+                    candidate.path.time,
+                    candidate.path.flow,
+                    candidate.path.total,
                 );
             }
 
             // Consider all the next steps.
             for (_, next) in valves.iter() {
-                if next.rate == 0 {
-                    continue;
-                }
-                if candidate.path[1..].contains(&next.name) {
-                    continue;
-                }
-                let current = candidate.path.last().unwrap();
-                let edge = (current.clone(), next.name.clone());
-                if let Some(dist) = adj.get(&edge) {
-                    let cost = dist + 1;
-                    let new_time = cost + candidate.time;
-                    if new_time >= 30 {
-                        continue;
-                    }
+                if let Some(new_path) =
+                    extend(&candidate.path, next, max_time, &candidate.seen, adj)
+                {
+                    best = best.max(new_path.score(max_time));
+                    more = true;
 
-                    // It passed all the tests. Try it.
-                    let addition = cost * candidate.flow;
-                    let new_total = candidate.total + addition;
-                    let new_flow = candidate.flow + next.rate;
-
-                    let mut new_path = candidate.path.clone();
-                    new_path.push(next.name.clone());
+                    let mut new_seen = candidate.seen.clone();
+                    new_seen.insert(next.name.clone());
 
                     new_candidates.push(Candidate {
                         path: new_path,
-                        time: new_time,
-                        flow: new_flow,
-                        total: new_total,
+                        seen: new_seen,
                     });
+                }
+            }
+        }
+        candidates = new_candidates;
+    }
+    best
+}
 
-                    let score = new_total + (new_flow * (30 - new_time));
-                    best = best.max(score);
+fn bfs_search2(
+    valves: &HashMap<String, Valve>,
+    adj: &HashMap<(String, String), i32>,
+    max_time: i32,
+    debug: bool,
+) -> i32 {
+    struct Candidate {
+        human: Path,
+        elephant: Path,
+        seen: HashSet<String>,
+    }
+    let empty = Candidate {
+        human: Path {
+            path: vec!["AA".to_string()],
+            time: 0,
+            flow: 0,
+            total: 0,
+        },
+        elephant: Path {
+            path: vec!["AA".to_string()],
+            time: 0,
+            flow: 0,
+            total: 0,
+        },
+        seen: HashSet::new(),
+    };
+    let mut candidates = Vec::new();
+    candidates.push(empty);
+    let mut more = true;
 
+    let mut best = 0;
+
+    while more {
+        more = false;
+
+        // Try extending any current candidate.
+        let mut new_candidates = Vec::new();
+        for candidate in candidates.iter() {
+            if debug {
+                println!(
+                    "Considering hum {} [time={}, flow={}, total={}]",
+                    candidate.human.path.join(" -> "),
+                    candidate.human.time,
+                    candidate.human.flow,
+                    candidate.human.total,
+                );
+                println!(
+                    "Considering ele {} [time={}, flow={}, total={}]",
+                    candidate.elephant.path.join(" -> "),
+                    candidate.elephant.time,
+                    candidate.elephant.flow,
+                    candidate.elephant.total,
+                );
+            }
+
+            // Consider all the next steps.
+            for (_, next) in valves.iter() {
+                if let Some(new_human) =
+                    extend(&candidate.human, next, max_time, &candidate.seen, adj)
+                {
+                    best = best.max(new_human.score(max_time) + candidate.elephant.score(max_time));
                     more = true;
+
+                    let mut new_seen = candidate.seen.clone();
+                    new_seen.insert(next.name.clone());
+
+                    let new_elephant = Path {
+                        path: candidate.elephant.path.clone(),
+                        time: candidate.elephant.time,
+                        flow: candidate.elephant.flow,
+                        total: candidate.elephant.total,
+                    };
+
+                    new_candidates.push(Candidate {
+                        human: new_human,
+                        elephant: new_elephant,
+                        seen: new_seen,
+                    });
+                }
+                if let Some(new_elephant) =
+                    extend(&candidate.elephant, next, max_time, &candidate.seen, adj)
+                {
+                    best = best.max(new_elephant.score(max_time) + candidate.human.score(max_time));
+                    more = true;
+
+                    let mut new_seen = candidate.seen.clone();
+                    new_seen.insert(next.name.clone());
+
+                    let new_human = Path {
+                        path: candidate.human.path.clone(),
+                        time: candidate.human.time,
+                        flow: candidate.human.flow,
+                        total: candidate.human.total,
+                    };
+
+                    new_candidates.push(Candidate {
+                        human: new_human,
+                        elephant: new_elephant,
+                        seen: new_seen,
+                    });
                 }
             }
         }
@@ -299,24 +372,11 @@ fn process(args: &Args) -> Result<()> {
     println!("building adjacency matrix...");
     let adj = build_adj(&valves, args.debug);
 
-    /*
-    println!("doing depth-first search...");
-    let current = "AA".to_string();
-    let current_flow = 0;
-    let time = 30;
-    let mut open = HashSet::new();
-    let flow = dfs_search(
-        &valves,
-        &adj,
-        &current,
-        current_flow,
-        time,
-        &mut open,
-        args.debug,
-    );
-    */
-
-    let ans = bfs_search(&valves, &adj, args.debug);
+    let ans = if args.part2 {
+        bfs_search2(&valves, &adj, 26, args.debug)
+    } else {
+        bfs_search1(&valves, &adj, 30, args.debug)
+    };
     println!("ans = {}", ans);
 
     Ok(())
