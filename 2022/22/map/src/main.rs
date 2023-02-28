ab8151e53bdb01141c919c89f5430b9e8c0be680
Bryan Klimt <bklimt@gmail.com>
2023-02-27 21:27:19 -0800

    day 22 part 2

--- 2022/22/map/src/main.rs
+++ 2022/22/map/src/main.rs
@@ -11,9 +11,12 @@ struct Args {
 
     #[arg(long)]
     debug: bool,
+
+    #[arg(long)]
+    part2: bool,
 }
 
-#[derive(Debug)]
+#[derive(Copy, Clone, Debug, PartialEq)]
 enum Orientation {
     Up,
     Down,
@@ -119,6 +122,236 @@ impl Map {
             (2, 3) => Face::Right,
             _ => panic!("bad face: {}, {}", latitude, longitude),
         }
+    }
+
+    /*
+                11F1
+                C11A
+                1111
+                1111
+        2F223C334444
+        D2223333444B
+        222233334444
+        2G2233E34444
+                555566B6
+                E5556666
+                5555666A
+                55G566D6
+    */
+
+    fn wrap_right(&self, pos: (usize, usize)) -> Option<((usize, usize), Orientation)> {
+        let face_size = self.rows.len() / 3;
+        let (x, y) = pos;
+        match self.face(pos) {
+            // A
+            Face::Top => {
+                if x == face_size * 3 - 1 {
+                    Some((
+                        ((face_size * 4) - 1, self.rows.len() - (y + 1)),
+                        Orientation::Left,
+                    ))
+                } else {
+                    None
+                }
+            }
+            // B
+            Face::Front => {
+                if x == face_size * 3 - 1 {
+                    Some((
+                        ((face_size * 4) - ((y - face_size) + 1), face_size * 2),
+                        Orientation::Down,
+                    ))
+                } else {
+                    None
+                }
+            }
+            // A
+            Face::Right => {
+                if x == face_size * 4 - 1 {
+                    Some((
+                        ((face_size * 3) - 1, (self.rows.len() - 1) - y),
+                        Orientation::Left,
+                    ))
+                } else {
+                    None
+                }
+            }
+            _ => None,
+        }
+    }
+
+    fn wrap_left(&self, pos: (usize, usize)) -> Option<((usize, usize), Orientation)> {
+        let face_size = self.rows.len() / 3;
+        let (x, y) = pos;
+        match self.face(pos) {
+            // C
+            Face::Top => {
+                if x == face_size * 2 {
+                    Some(((face_size + y, face_size), Orientation::Down))
+                } else {
+                    None
+                }
+            }
+            // D
+            Face::Back => {
+                if x == 0 {
+                    Some((
+                        (
+                            ((face_size * 4) - 1) - (y - face_size),
+                            (self.rows.len()) - 1,
+                        ),
+                        Orientation::Up,
+                    ))
+                } else {
+                    None
+                }
+            }
+            // E
+            Face::Down => {
+                if x == face_size * 2 {
+                    Some((
+                        (face_size + ((face_size * 3) - (y + 1)), face_size * 2 - 1),
+                        Orientation::Up,
+                    ))
+                } else {
+                    None
+                }
+            }
+            _ => None,
+        }
+    }
+
+    fn wrap_up(&self, pos: (usize, usize)) -> Option<((usize, usize), Orientation)> {
+        let face_size = self.rows.len() / 3;
+        let (x, y) = pos;
+        match self.face(pos) {
+            // F
+            Face::Top => {
+                if y == 0 {
+                    Some((((face_size * 3) - (x + 1), face_size), Orientation::Down))
+                } else {
+                    None
+                }
+            }
+            // F
+            Face::Back => {
+                if y == face_size {
+                    Some((((face_size * 3) - (x + 1), 0), Orientation::Down))
+                } else {
+                    None
+                }
+            }
+            // C
+            Face::Left => {
+                if y == face_size {
+                    Some(((face_size * 2, x - face_size), Orientation::Right))
+                } else {
+                    None
+                }
+            }
+            // B
+            Face::Right => {
+                if y == face_size * 2 {
+                    Some((
+                        (face_size * 3 - 1, face_size + (face_size * 4) - (x + 1)),
+                        Orientation::Left,
+                    ))
+                } else {
+                    None
+                }
+            }
+            _ => None,
+        }
+    }
+
+    fn wrap_down(&self, pos: (usize, usize)) -> Option<((usize, usize), Orientation)> {
+        let face_size = self.rows.len() / 3;
+        let (x, y) = pos;
+        match self.face(pos) {
+            // G
+            Face::Back => {
+                if y == face_size * 2 - 1 {
+                    Some((
+                        ((face_size * 3) - (x + 1), self.rows.len() - 1),
+                        Orientation::Up,
+                    ))
+                } else {
+                    None
+                }
+            }
+            // E
+            Face::Left => {
+                if y == face_size * 2 - 1 {
+                    Some((
+                        (face_size * 2, ((face_size * 2) + (face_size * 2) - (x + 1))),
+                        Orientation::Right,
+                    ))
+                } else {
+                    None
+                }
+            }
+            // G
+            Face::Down => {
+                if y == self.rows.len() - 1 {
+                    Some((
+                        ((face_size * 3) - (x + 1), face_size * 2 - 1),
+                        Orientation::Up,
+                    ))
+                } else {
+                    None
+                }
+            }
+            // D
+            Face::Right => {
+                if y == self.rows.len() - 1 {
+                    Some((
+                        (0, face_size + (face_size * 4) - (x + 1)),
+                        Orientation::Right,
+                    ))
+                } else {
+                    None
+                }
+            }
+            _ => None,
+        }
+    }
+
+    fn wrap(
+        &self,
+        pos: (usize, usize),
+        dir: &Orientation,
+    ) -> Option<((usize, usize), Orientation)> {
+        match dir {
+            Orientation::Up => self.wrap_up(pos),
+            Orientation::Down => self.wrap_down(pos),
+            Orientation::Left => self.wrap_left(pos),
+            Orientation::Right => self.wrap_right(pos),
+        }
+    }
+
+    fn walk2(
+        &self,
+        pos: (usize, usize),
+        dir: &Orientation,
+        dist: i64,
+    ) -> ((usize, usize), Orientation) {
+        let mut pos = pos;
+        let mut dir = *dir;
+        for _ in 0..dist {
+            let (new_pos, new_dir) = self.wrap(pos, &dir).unwrap_or_else(|| match dir {
+                Orientation::Up => ((pos.0, pos.1 - 1), Orientation::Up),
+                Orientation::Down => ((pos.0, pos.1 + 1), Orientation::Down),
+                Orientation::Left => ((pos.0 - 1, pos.1), Orientation::Left),
+                Orientation::Right => ((pos.0 + 1, pos.1), Orientation::Right),
+            });
+            let new_row = self.rows.get(new_pos.1).unwrap();
+            if new_row.data[new_pos.0 - new_row.offset] {
+                break;
+            }
+            pos = new_pos;
+            dir = new_dir;
+        }
+        (pos, dir)
     }
 
     fn print(&self, pos: (usize, usize), dir: &Orientation) {
@@ -227,12 +460,34 @@ impl Map {
         pos
     }
 
-    fn walk(&self, pos: (usize, usize), dir: &Orientation, dist: i64) -> (usize, usize) {
-        match dir {
-            Orientation::Up => self.walk_up(pos, dist),
-            Orientation::Down => self.walk_down(pos, dist),
-            Orientation::Left => self.walk_left(pos, dist),
-            Orientation::Right => self.walk_right(pos, dist),
+    fn walk1(
+        &self,
+        pos: (usize, usize),
+        dir: &Orientation,
+        dist: i64,
+    ) -> ((usize, usize), Orientation) {
+        (
+            match dir {
+                Orientation::Up => self.walk_up(pos, dist),
+                Orientation::Down => self.walk_down(pos, dist),
+                Orientation::Left => self.walk_left(pos, dist),
+                Orientation::Right => self.walk_right(pos, dist),
+            },
+            *dir,
+        )
+    }
+
+    fn walk(
+        &self,
+        pos: (usize, usize),
+        dir: &Orientation,
+        dist: i64,
+        part2: bool,
+    ) -> ((usize, usize), Orientation) {
+        if part2 {
+            self.walk2(pos, dir, dist)
+        } else {
+            self.walk1(pos, dir, dist)
         }
     }
 }
@@ -333,7 +588,9 @@ fn read_input(path: &str, _debug: bool) -> Result<(Map, Vec<Instruction>)> {
 }
 
 fn process(args: &Args) -> Result<()> {
-    println!("reading input...");
+    if args.debug {
+        println!("reading input...");
+    }
     let (map, instructions) = read_input(&args.input, args.debug)?;
 
     let mut pos = map.initial_pos();
@@ -341,6 +598,8 @@ fn process(args: &Args) -> Result<()> {
     if args.debug {
         map.print(pos, &dir);
         println!("{:?}", instructions);
+        println!("rows = {}", map.rows.len());
+        println!("face_size = {}", map.rows.len() / 3);
     }
 
     for step in instructions {
@@ -350,7 +609,7 @@ fn process(args: &Args) -> Result<()> {
         }
         match step {
             Instruction::Forward(dist) => {
-                pos = map.walk(pos, &dir, dist);
+                (pos, dir) = map.walk(pos, &dir, dist, args.part2);
             }
             Instruction::Right => {
                 dir = dir.turn_right();
@@ -380,4 +639,51 @@ fn main() {
         Ok(_) => (),
         Err(error) => panic!("{:?}", error),
     };
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_wrap() {
+        let (m, _) = read_input("./empty4x4.txt", true).unwrap();
+        assert_eq!(m.rows.len(), 12);
+
+        let cases: Vec<(usize, usize, Orientation, usize, usize, Orientation)> = vec![
+            (8, 0, Orientation::Right, 9, 0, Orientation::Right),
+            // A
+            (11, 0, Orientation::Right, 15, 11, Orientation::Left),
+            (15, 11, Orientation::Right, 11, 0, Orientation::Left),
+            // B
+            (11, 5, Orientation::Right, 14, 8, Orientation::Down),
+            (14, 8, Orientation::Up, 11, 5, Orientation::Left),
+            // C
+            (8, 1, Orientation::Left, 5, 4, Orientation::Down),
+            (5, 4, Orientation::Up, 8, 1, Orientation::Right),
+            // D
+            (0, 5, Orientation::Left, 14, 11, Orientation::Up),
+            (14, 11, Orientation::Down, 0, 5, Orientation::Right),
+            // E
+            (8, 9, Orientation::Left, 6, 7, Orientation::Up),
+            (6, 7, Orientation::Down, 8, 9, Orientation::Right),
+            // F
+            (1, 4, Orientation::Up, 10, 0, Orientation::Down),
+            (10, 0, Orientation::Up, 1, 4, Orientation::Down),
+            // G
+            (1, 7, Orientation::Down, 10, 11, Orientation::Up),
+            (10, 11, Orientation::Down, 1, 7, Orientation::Up),
+        ];
+        for (i, case) in cases.iter().enumerate() {
+            let (start_x, start_y, start_dir, exp_x, exp_y, exp_dir) = case;
+            let ((act_x, act_y), act_dir) = m.walk((*start_x, *start_y), start_dir, 1, true);
+            println!(
+                "case {}: walking from ({}, {}) {:?}: = ({}, {}) {:?}; want ({} {}) {:?};",
+                i, start_x, start_y, &start_dir, act_x, act_y, &act_dir, exp_x, exp_y, &exp_dir,
+            );
+            assert_eq!(act_x, *exp_x);
+            assert_eq!(act_y, *exp_y);
+            assert_eq!(act_dir, *exp_dir);
+        }
+    }
 }
