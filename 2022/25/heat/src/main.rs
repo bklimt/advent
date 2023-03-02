45e39c83a2138e4779a716f7593febaaee5f88d6
Bryan Klimt <bklimt@gmail.com>
2023-03-01 16:25:43 -0800

    day 25 part 1

--- 2022/25/heat/src/main.rs
+++ 2022/25/heat/src/main.rs
@@ -1,4 +1,4 @@
-use anyhow::{Context, Result};
+use anyhow::{anyhow, Context, Result};
 use clap::Parser;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
@@ -13,9 +13,38 @@ struct Args {
     debug: bool,
 }
 
-fn read_input(path: &str, debug: bool) -> Result<()> {
+fn decode(s: &str) -> Result<i64> {
+    let mut place = 1i64;
+    let mut pos = 0i64;
+    let mut neg = 0i64;
+    for c in s.chars().rev() {
+        match c {
+            '2' => {
+                pos = pos + 2 * place;
+            }
+            '1' => {
+                pos = pos + place;
+            }
+            '0' => {}
+            '-' => {
+                neg = neg + place;
+            }
+            '=' => {
+                neg = neg + 2 * place;
+            }
+            _ => {
+                return Err(anyhow!("invalid character: {}", c));
+            }
+        }
+        place = place * 5;
+    }
+    Ok(pos - neg)
+}
+
+fn read_input(path: &str, debug: bool) -> Result<Vec<i64>> {
     let file = File::open(path).with_context(|| format!("unable to open file {:?}", path))?;
     let mut r = BufReader::new(file);
+    let mut v = Vec::new();
     loop {
         let mut line = String::new();
         let n = r.read_line(&mut line).unwrap();
@@ -28,15 +57,16 @@ fn read_input(path: &str, debug: bool) -> Result<()> {
             continue;
         }
 
+        let n = decode(line)?;
         if debug {
-            println!("line: {}", line);
+            println!("{} => {}", line, n);
         }
+        v.push(n);
     }
-    Ok(())
+    Ok(v)
 }
 
 fn process(args: &Args) -> Result<()> {
-    println!("reading input...");
     let _ = read_input(&args.input, args.debug)?;
     Ok(())
 }
