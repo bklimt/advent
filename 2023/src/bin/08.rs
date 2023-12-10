8cad046b7008ce5ee093489a3a7e1476f722c8ff
Bryan Klimt <bklimt@gmail.com>
2023-12-09 18:43:50 -0800

    day 8

--- 2023/src/bin/08.rs
+++ 2023/src/bin/08.rs
@@ -1,6 +1,7 @@
 use anyhow::{anyhow, Context, Result};
 use clap::Parser;
-use std::collections::HashMap;
+use num::integer::lcm;
+use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::option::Option;
@@ -27,75 +28,147 @@ struct Input {
     map: HashMap<String, (String, String)>,
 }
 
-fn read_input(path: &str, _debug: bool) -> Result<Input> {
-    let file = File::open(path).with_context(|| format!("unable to open file {:?}", path))?;
-    let mut r = BufReader::new(file);
-    let mut directions: Option<String> = None;
-    let mut map = HashMap::new();
-    loop {
-        let mut line = String::new();
-        let n = r.read_line(&mut line).unwrap();
-        let line = line.trim();
+impl Input {
+    fn read(path: &str, _debug: bool) -> Result<Self> {
+        let file = File::open(path).with_context(|| format!("unable to open file {:?}", path))?;
+        let mut r = BufReader::new(file);
+        let mut directions: Option<String> = None;
+        let mut map = HashMap::new();
+        loop {
+            let mut line = String::new();
+            let n = r.read_line(&mut line).unwrap();
+            let line = line.trim();
 
-        if line == "" {
-            if n == 0 {
-                break;
+            if line == "" {
+                if n == 0 {
+                    break;
+                }
+                continue;
             }
-            continue;
+
+            if directions.is_none() {
+                directions = Some(line.to_owned());
+            } else {
+                // AAA = (BBB, CCC)
+                if line.len() != 16 {
+                    return Err(anyhow!("invalid map line: {}", line));
+                }
+                let start = line[..3].to_owned();
+                let left = line[7..10].to_owned();
+                let right = line[12..15].to_owned();
+                map.insert(start, (left, right));
+            }
         }
+        let directions = directions.context("missing directions")?;
+        let directions: Result<Vec<Direction>> = directions
+            .chars()
+            .map(|c| match c {
+                'L' => Ok(Direction::Left),
+                'R' => Ok(Direction::Right),
+                _ => Err(anyhow!("invalid direction: {}", c)),
+            })
+            .collect();
+        let directions = directions?;
 
-        if directions.is_none() {
-            directions = Some(line.to_owned());
-        } else {
-            // AAA = (BBB, CCC)
-            if line.len() != 16 {
-                return Err(anyhow!("invalid map line: {}", line));
+        Ok(Input { directions, map })
+    }
+
+    fn part1(&self) -> Result<u64> {
+        let mut total = 0;
+        let mut i = 0;
+        let mut current: String = "AAA".to_owned();
+        while current != "ZZZ" {
+            let (left, right) = self
+                .map
+                .get(&current)
+                .context(format!("unknown position: {}", current))?;
+
+            current = match self.directions[i] {
+                Direction::Left => left.clone(),
+                Direction::Right => right.clone(),
+            };
+
+            total += 1;
+            i = (i + 1) % self.directions.len();
+        }
+        Ok(total)
+    }
+
+    fn find_cycle(&self, start: String, debug: bool) -> Result<u64> {
+        // Map of (direction index, location) -> steps taken.
+        let mut seen: HashMap<(usize, String), u64> = HashMap::new();
+        // The step and name of any destination we've seen.
+        let mut z: Option<(u64, String)> = None;
+        let mut step: u64 = 0;
+        let mut current: String = start;
+        loop {
+            let i: usize = (step as usize) % self.directions.len();
+
+            if let Some(prev) = seen.get(&(i, current.clone())) {
+                if let Some((dest_step, dest)) = z {
+                    if debug {
+                        println!(
+                            "loop from {} to {} with dest {} at {}",
+                            step, *prev, dest, dest_step
+                        );
+                    }
+                    if step - dest_step != *prev {
+                        return Err(anyhow!("cycle would have non-zero offset"));
+                    }
+                    return Ok(dest_step);
+                } else {
+                    return Err(anyhow!("loop without a dest!"));
+                }
             }
-            let start = line[..3].to_owned();
-            let left = line[7..10].to_owned();
-            let right = line[12..15].to_owned();
-            map.insert(start, (left, right));
+            seen.insert((i, current.clone()), step);
+
+            if current.ends_with("Z") {
+                match z {
+                    Some((s, n)) => {
+                        return Err(anyhow!(
+                            "at {}, {}, but already saw {}, {}",
+                            step,
+                            current,
+                            s,
+                            n
+                        ));
+                    }
+                    None => {
+                        z = Some((step, current.clone()));
+                    }
+                }
+            }
+
+            let (left, right) = self
+                .map
+                .get(&current)
+                .context(format!("unknown position: {}", current))?;
+
+            current = match self.directions[i] {
+                Direction::Left => left.clone(),
+                Direction::Right => right.clone(),
+            };
+
+            step += 1;
         }
     }
-    let directions = directions.context("missing directions")?;
-    let directions: Result<Vec<Direction>> = directions
-        .chars()
-        .map(|c| match c {
-            'L' => Ok(Direction::Left),
-            'R' => Ok(Direction::Right),
-            _ => Err(anyhow!("invalid direction: {}", c)),
-        })
-        .collect();
-    let directions = directions?;
 
-    Ok(Input { directions, map })
-}
-
-fn traverse(input: &Input) -> Result<u64> {
-    let mut total = 0;
-    let mut i = 0;
-    let mut current: &String = &"AAA".to_owned();
-    while current != "ZZZ" {
-        let (left, right) = input
-            .map
-            .get(current)
-            .context(format!("unknown position: {}", current))?;
-
-        current = match input.directions[i] {
-            Direction::Left => left,
-            Direction::Right => right,
-        };
-
-        total += 1;
-        i = (i + 1) % input.directions.len();
+    fn part2(&self, debug: bool) -> Result<u64> {
+        let mut ans: u64 = 1;
+        for node in self.map.keys() {
+            if node.ends_with("A") {
+                let cycle = self.find_cycle(node.clone(), debug)?;
+                ans = lcm(ans, cycle);
+            }
+        }
+        Ok(ans)
     }
-    Ok(total)
 }
 
 fn process(args: &Args) -> Result<()> {
-    let input = read_input(&args.input, args.debug)?;
-    let ans = traverse(&input)?;
-    println!("ans1: {}", ans);
+    let input = Input::read(&args.input, args.debug)?;
+    println!("ans1: {}", input.part1()?);
+    println!("ans2: {}", input.part2(args.debug)?);
     Ok(())
 }
 
