b36588bd8ec8ad219fa722f1208ab9de3e4eebf4
Bryan Klimt <bklimt@gmail.com>
2023-12-20 19:48:07 -0800

    day 20 part 1

--- 2023/src/bin/20.rs
+++ 2023/src/bin/20.rs
@@ -2,20 +2,62 @@ use advent::common::{read_lines, StrIterator};
 use anyhow::{Context, Result};
 use clap::Parser;
 use itertools::Itertools;
-use std::{collections::HashMap, str::FromStr};
+use std::{
+    collections::{HashMap, VecDeque},
+    fmt::Display,
+    str::FromStr,
+};
+
+#[derive(Debug, Clone, Copy)]
+enum SignalLevel {
+    Low,
+    High,
+}
+
+impl Display for SignalLevel {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                SignalLevel::Low => "low",
+                SignalLevel::High => "high",
+            }
+        )
+    }
+}
+
+#[derive(Debug)]
+struct Signal {
+    sender: String,
+    receiver: String,
+    level: SignalLevel,
+}
+
+impl Display for Signal {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} -{}-> {}", self.sender, self.level, self.receiver,)
+    }
+}
 
 #[derive(Debug)]
 enum ModuleType {
     Broadcaster,
-    FlipFlop,
-    Conjunction,
+    FlipFlop {
+        on: bool,
+    },
+    Conjunction {
+        states: HashMap<String, SignalLevel>,
+    },
+    Output,
 }
 
 #[derive(Debug)]
 struct Module {
     typ: ModuleType,
     name: String,
-    destinations: Vec<String>,
+    inputs: Vec<String>,
+    outputs: Vec<String>,
 }
 
 impl FromStr for Module {
@@ -32,17 +74,56 @@ impl FromStr for Module {
             .nth(0)
             .with_context(|| format!("module lhs is empty: {:?}", s))?;
         let (typ, name) = match c {
-            '%' => (ModuleType::FlipFlop, &lhs[1..]),
-            '&' => (ModuleType::Conjunction, &lhs[1..]),
+            '%' => (ModuleType::FlipFlop { on: false }, &lhs[1..]),
+            '&' => (
+                ModuleType::Conjunction {
+                    states: HashMap::new(),
+                },
+                &lhs[1..],
+            ),
             _ => (ModuleType::Broadcaster, lhs),
         };
         let name = name.to_owned();
-        let destinations: Vec<String> = rhs.split(", ").map(|s| s.to_owned()).collect_vec();
+        let inputs = Vec::new();
+        let outputs = rhs.split(", ").map(|s| s.to_owned()).collect_vec();
         Ok(Module {
             typ,
             name,
-            destinations,
+            inputs,
+            outputs,
         })
+    }
+}
+
+// Fills in the input fields as the inverse of the output fields.
+fn initialize_inputs(modules: &mut HashMap<String, Module>) {
+    let mut index: HashMap<String, Vec<String>> = HashMap::new();
+    for (_, module) in modules.iter() {
+        for output in module.outputs.iter() {
+            if !index.contains_key(output) {
+                index.insert(output.clone(), Vec::new());
+            }
+            let v = index.get_mut(output).expect("just checked");
+            v.push(module.name.clone());
+        }
+    }
+
+    for (_, module) in modules.iter_mut() {
+        if let Some(inputs) = index.get(&module.name) {
+            for input in inputs {
+                module.inputs.push(input.clone());
+            }
+        }
+    }
+}
+
+fn initialize_conjunctions(modules: &mut HashMap<String, Module>) {
+    for (_, module) in modules.iter_mut() {
+        if let ModuleType::Conjunction { states } = &mut module.typ {
+            for input in module.inputs.iter() {
+                states.insert(input.clone(), SignalLevel::Low);
+            }
+        }
     }
 }
 
@@ -52,7 +133,83 @@ fn read_input(path: &str) -> Result<HashMap<String, Module>> {
     for module in modules {
         map.insert(module.name.clone(), module);
     }
+    map.insert(
+        "output".to_owned(),
+        Module {
+            typ: ModuleType::Output,
+            name: "output".to_owned(),
+            inputs: Vec::new(),
+            outputs: Vec::new(),
+        },
+    );
+    initialize_inputs(&mut map);
+    initialize_conjunctions(&mut map);
     Ok(map)
+}
+
+fn run(modules: &mut HashMap<String, Module>, debug: bool) -> Result<u64> {
+    let mut low = 0;
+    let mut high = 0;
+    for _ in 0..1000 {
+        let mut q = VecDeque::new();
+        q.push_back(Signal {
+            sender: "button".to_owned(),
+            receiver: "broadcaster".to_owned(),
+            level: SignalLevel::Low,
+        });
+        while let Some(signal) = q.pop_front() {
+            if debug {
+                println!("{}", signal);
+            }
+            match &signal.level {
+                SignalLevel::Low => low += 1,
+                SignalLevel::High => high += 1,
+            }
+            if let Some(module) = modules.get_mut(&signal.receiver) {
+                let output_level = match &mut module.typ {
+                    ModuleType::FlipFlop { on } => {
+                        if let SignalLevel::Low = signal.level {
+                            *on = !*on;
+                            if *on {
+                                SignalLevel::High
+                            } else {
+                                SignalLevel::Low
+                            }
+                        } else {
+                            continue;
+                        }
+                    }
+                    ModuleType::Conjunction { states } => {
+                        states.insert(signal.sender.clone(), signal.level);
+                        let mut all_high = true;
+                        for (_, level) in states.iter() {
+                            if let SignalLevel::Low = level {
+                                all_high = false;
+                                break;
+                            }
+                        }
+                        if all_high {
+                            SignalLevel::Low
+                        } else {
+                            SignalLevel::High
+                        }
+                    }
+                    ModuleType::Broadcaster => signal.level,
+                    ModuleType::Output { .. } => {
+                        continue;
+                    }
+                };
+                for output in module.outputs.iter() {
+                    q.push_back(Signal {
+                        sender: signal.receiver.clone(),
+                        receiver: output.clone(),
+                        level: output_level.clone(),
+                    });
+                }
+            }
+        }
+    }
+    Ok(low * high)
 }
 
 #[derive(Parser, Debug)]
@@ -66,10 +223,17 @@ struct Args {
 }
 
 fn process(args: &Args) -> Result<()> {
-    let input = read_input(args.input.as_str())?;
-    for (_, module) in input.iter() {
-        println!("{:?}", module);
+    let mut modules = read_input(args.input.as_str())?;
+    if args.debug {
+        for (_, module) in modules.iter() {
+            println!("{:?}", module);
+        }
+        println!("");
     }
+
+    let ans1 = run(&mut modules, args.debug)?;
+    println!("ans1 = {}", ans1);
+
     Ok(())
 }
 
